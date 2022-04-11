ece220131d9c469ee2c0273d7fbf03ca69903b01
xiaguan <751080330@qq.com>
2022-04-11 23:21:39 +0800

    LHY_HW_01

---
+++ func.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+def use_svg_display():
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5,2.5)):
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
